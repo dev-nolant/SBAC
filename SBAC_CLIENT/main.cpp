8a9c4f072c8fbda67137cc5b821b2d1237bf4162
Nolan Taft <killer123yo@gmail.com>
2024-04-01 13:20:38 -0600

    Add files via upload

--- SBAC_CLIENT/main.cpp
+++ SBAC_CLIENT/main.cpp
@@ -1,6 +1,11 @@
 #include <winsock2.h>
 #include <ws2tcpip.h>
 #include <iostream>
+#include "lib/nlohmann/json.hpp"
+
+// for convenience
+using json = nlohmann::json;
+using namespace std;
 
 #pragma comment(lib, "Ws2_32.lib")
 
@@ -11,39 +16,52 @@ int main() {
     int resultCode = WSAStartup(MAKEWORD(2,2), &wsaData);
 
     if (resultCode != 0) {
-        std::cout << "WSAStartup failed: " << resultCode << std::endl;
+        cout << "WSAStartup failed: " << resultCode << endl;
         return 1;
     }
 
     Socket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     struct hostent *host;
-    host = gethostbyname("localhost"); // Removed port from here
+    host = gethostbyname("localhost");
 
-    SockAddr.sin_port = htons(8080); // Correct port number
+    SockAddr.sin_port = htons(8080);
     SockAddr.sin_family = AF_INET;
     SockAddr.sin_addr.s_addr = *((unsigned long*)host->h_addr);
 
     if (connect(Socket, (SOCKADDR*)(&SockAddr), sizeof(SockAddr)) != 0) {
-        std::cout << "Could not connect" << std::endl;
+        cout << "Could not connect" << endl;
         return 1;
     }
 
-    // Adjusted GET request for /detections endpoint
     const char* httpRequest = "GET /detections HTTP/1.1\r\nHost: localhost\r\nConnection: close\r\n\r\n";
     send(Socket, httpRequest, strlen(httpRequest), 0);
 
-    char buffer[10000];
+    string response;
+    char buffer[512];
     int nDataLength;
     while ((nDataLength = recv(Socket, buffer, sizeof(buffer), 0)) > 0) {
-        int i = 0;
-        while (i < nDataLength) {
-            std::cout << buffer[i];
-            i++;
-        }
+        response.append(buffer, nDataLength);
     }
 
     closesocket(Socket);
     WSACleanup();
+
+    // Find the start of the JSON part (skip HTTP headers)
+    auto jsonStartPos = response.find("\r\n\r\n");
+    if (jsonStartPos != string::npos) {
+        try {
+            auto jsonResponse = json::parse(response.substr(jsonStartPos + 4)); // Parse JSON
+
+            for (const auto& item : jsonResponse) {
+                for (auto& element : item.items()) {
+                    cout << element.key() << ": " << element.value() << endl;
+                }
+                cout << "-----------------------" << endl; // Separator between objects
+            }
+        } catch (const json::parse_error& e) {
+            cout << "Parsing error: " << e.what() << "\n";
+        }
+    }
 
     return 0;
 }
